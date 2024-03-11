8f662f3388777a35c1bf0ab6c211d529df8b83dc
Vineetha Pattapagalu <59002685+vineethapattapagalu@users.noreply.github.com>
2024-03-11 17:41:12 -0600

    Time: 164 ms (22.52%), Space: 13.2 MB (71.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,11 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums_dict = {}
+        
+        dic = {}
         for i in nums:
-            if i not in nums_dict:
-                nums_dict[i] = 1
+            if i not in dic :
+                dic[i] = 1
             else:
-                nums_dict[i] += 1
-        result = sorted(nums_dict.items(), key = lambda x:x[1] )
-        return result[-1][0]
+                dic[i] += 1
+            if dic[i] > len(nums)//2:
+                return i
