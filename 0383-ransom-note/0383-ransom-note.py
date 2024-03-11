da85d1344d88a29d2a298de5fbcdfbb3df93044e
Vineetha Pattapagalu <59002685+vineethapattapagalu@users.noreply.github.com>
2024-03-11 17:54:39 -0600

    Time: 39 ms (73.45%), Space: 11.6 MB (95.39%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -6,23 +6,18 @@ class Solution(object):
         :rtype: bool
         """
         
-        ransomeNote_dict = {}
-        magazine_dict = {}
-        
+        dic = {}
         for i in magazine:
-            if i not in magazine_dict:
-                magazine_dict[i] = 1
+            if i not in dic:
+                dic[i] = 1
             else:
-                magazine_dict[i] += 1
+                dic[i] += 1
         for i in ransomNote:
-            if i not in ransomeNote_dict:
-                ransomeNote_dict[i] = 1
-            else:
-                ransomeNote_dict[i] += 1
-        for key, value in ransomeNote_dict.items():
-            if key in magazine_dict and value <= magazine_dict[key]:
-                continue
-            else:
+            if i not in dic:
                 return False
+            else:
+                dic[i] -= 1
+                if dic[i] < 0:
+                    return False
         return True
         
