fec9fbdf365b8ffdf1539f05cb40d9958d51bdd5
Vineetha Pattapagalu <59002685+vineethapattapagalu@users.noreply.github.com>
2024-03-13 13:20:39 -0600

    Time: 28 ms (87.21%), Space: 11.8 MB (30.00%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.py
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -4,14 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        arr = []
-        while nums:
-            min_element_alice = min(nums)
-            nums.remove(min_element_alice)
-            min_element_bob = min(nums)
-            nums.remove(min_element_bob)
-            arr.append(min_element_bob)
-            arr.append(min_element_alice)
+#         arr = []
+#         while nums:
+#             min_element_alice = min(nums)
+#             nums.remove(min_element_alice)
+#             min_element_bob = min(nums)
+#             nums.remove(min_element_bob)
+#             arr.append(min_element_bob)
+#             arr.append(min_element_alice)
             
+#         return arr
+
+        nums.sort()
+        arr = []
+        for i in range(0, len(nums)-1, 2):
+            arr.append(nums[i+1])
+            arr.append(nums[i])
         return arr
         
