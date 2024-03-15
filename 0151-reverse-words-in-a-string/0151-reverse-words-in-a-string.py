fcf40335b502b651a48273b122ab4fe1b1c7a8f8
Vineetha Pattapagalu <59002685+vineethapattapagalu@users.noreply.github.com>
2024-03-15 17:48:05 -0600

    Time: 20 ms (33.79%), Space: 12.1 MB (37.02%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -5,12 +5,26 @@ class Solution(object):
         :rtype: str
         """
         
-        split_s = s.split(' ')
-        result = []
-        for i in split_s:
-            if i == '':
-                continue
-            result.append(i)
-        print(result)
-        return ' '.join(result[::-1])
+#         split_s = s.split(' ')
+#         result = []
+#         for i in split_s:
+#             if i == '':
+#                 continue
+#             result.append(i)
+#         return ' '.join(result[::-1])
         
+        s_list = []
+        
+        temp = ''
+        for i in s:
+            if i == ' ':
+                if temp != '':
+                    s_list.append(temp)
+                temp = ''
+            else:
+                temp += i
+        if temp != '':
+            s_list.append(temp)
+        print(s_list)
+        s_list = s_list[::-1]
+        return ' '.join(s_list)
